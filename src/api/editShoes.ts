312c3a61b17ba82256105054bc6897ac0d12957e
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-12-26 21:05:03 +0100

    add axios config

--- src/api/editShoes.ts
+++ src/api/editShoes.ts
@@ -1,12 +1,11 @@
 // @ts-nocheck
-import axios from "axios";
+import { instance } from "../config/axios";
 import { DrawerForm } from "../types";
 
 export const editShoes = async (
   shoes: DrawerForm,
   shoesId: string
 ): Promise<any> => {
-  const URL = `https://shoes-app-back.vercel.app/all-shoes/${shoesId}`;
   delete shoes._id;
-  await axios.put(URL, shoes)
+  await instance.put(`${shoesId}`, shoes)
 };
--- src/api/getAllShoes.ts
+++ src/api/getAllShoes.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import { instance } from '../config/axios';
 
 const URL = `https://shoes-app-back.vercel.app/all-shoes`;
 
 export const getAllShoes = async (): Promise<any> => {
-  const result = await axios.get(URL);
+  //const result = await axios.get(URL);
+  const result = await instance.get("")
   return result?.data || [];
 };
