1553c3b8944cf83b42c8095487d38bb1bb632671
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-12-26 20:32:13 +0100

    add new endpoints

--- src/api/deleteShoes.ts
+++ src/api/deleteShoes.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { KEY } from "../constants";
 
 export const deleteShoes = async (shoesId: string): Promise<any> => {
-  const URL = `https://crudcrud.com/api/${KEY}/shoes/${shoesId}`;
+  const URL = `https://shoes-app-back.vercel.app/all-shoes/${shoesId}`;
   await axios.delete(URL);
 };
--- src/api/editShoes.ts
+++ src/api/editShoes.ts
@@ -1,21 +1,12 @@
+// @ts-nocheck
 import axios from "axios";
 import { DrawerForm } from "../types";
-import { KEY } from "../constants";
 
 export const editShoes = async (
   shoes: DrawerForm,
   shoesId: string
 ): Promise<any> => {
-  const URL = `https://crudcrud.com/api/${KEY}/shoes/${shoesId}`;
-  console.log(shoesId, shoes, "API");
-  await fetch(URL, {
-    method: "PUT",
-    body: JSON.stringify(shoes),
-    mode: "no-cors",
-    credentials: "include",
-    headers:
-      typeof Headers !== "undefined"
-        ? new Headers({ "content-type": "application/x-www-form-urlencoded" })
-        : { "content-type": "application/x-www-form-urlencoded" },
-  });
+  const URL = `https://shoes-app-back.vercel.app/all-shoes/${shoesId}`;
+  delete shoes._id;
+  await axios.put(URL, shoes)
 };
